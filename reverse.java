a54084eb018b462115a46a56ead658186f6e2f9e
Renuka Choudhary <90044817+renu-run-run@users.noreply.github.com>
2021-10-22 11:36:27 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,13 @@
+package simple;
+
+public class reverse {
+  public static void main(String[] args) {
+	  int num = 12345, rev = 0;
+	  while ( num!=0) {
+		  int rem = num%10;
+		   rev = rev*10+rem;
+		   num = num/10;
+	  }
+	  System.out.println("reverse number = "+ rev);
+  }
+}
